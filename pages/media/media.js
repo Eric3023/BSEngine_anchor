04cb4a5294918885a5779dd249c758d8a396d3b2
Eric3023 <Mm5211314mM>
2020-08-17 16:09:58 +0800

    账号列表页面和添加账号页面

---
+++ pages/media/media.js
@@ -0,0 +1,44 @@
+// pages/media/media.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    type: 0,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 更改类型
+   */
+  onChangeType: function (event) {
+    this.setData({
+      type: event.currentTarget.dataset.type
+    })
+    console.log(`type:${this.data.type}`);
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  /**
+   * 添加账号
+   */
+  addAccount: function (event) {
+    wx.navigateTo({
+      url: '/pages/addAccount/addAccount',
+    })
+  }
+})
