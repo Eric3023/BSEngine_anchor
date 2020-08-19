5bd388a3479545d6f1a8075dbf30808e3c214e3e
donghuadong <827142667@qq.com>
2020-08-19 13:45:51 +0800

    抢单成功

--- pages/order_detail/order_detail.js
+++ pages/order_detail/order_detail.js
@@ -11,9 +11,23 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let value = options.detail;
-    this.setData({
-      data: JSON.parse(value),
-    });
+
+  },
+
+  /**
+   * 点击了抢单
+   */
+  onRob: function(event){
+    wx.showModal({
+      content: '抢单成功！\n请在订单里查看执行',
+      cancelText: '返回',
+      success (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })    
   },
 })
